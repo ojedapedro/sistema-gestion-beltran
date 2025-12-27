16ec31bff27df57c46dbf4ffe74c424b9074c185
ANALISTA DE DATOS <ojedapedro2302741@gmail.com>
2025-12-27 17:00:00 -0400

    mod1

--- constants.ts
+++ constants.ts
@@ -13,6 +13,6 @@ export const GOOGLE_SHEET_ID = '1vhTFY-DLkHZIvTozAj-_ZiJDLftgkHmh494OM9EjDdQ';
 
 // Nota: El usuario debe desplegar el script setup_sheets.gs como Web App y pegar la URL aquí.
 // Por ahora, el service manejará un fallback a localStorage si no hay URL.
-export const WEB_APP_URL = ''; 
+export const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby4qzP_CCbCpUAwLbHljlWBPRT7a1hftjLRSxX3CFpArsw7LW1pnDc-QFgaD2g6GHWDaA/exec'; 
 
 export const SECTIONS = ['A', 'B', 'C', 'U'];
